b246303463b57b4bfd00387d1111c2c21156eb77
Yeung Tsz Yan <yannieyeung@Yeungs-MacBook-Pro.local>
2020-02-18 15:08:31 +0800

    created game logic

---
+++ memory-game/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+let cardOne = "queen";
+let cardTwo = "king";
+let cardThree = "king";
+let cardFour = "queen";
+console.log("User flipped " + cardOne);
+console.log("User flipped" + cardTwo);
