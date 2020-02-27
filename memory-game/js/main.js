9cf0d39ee4b71e38645012ce785f003b286963d1
Yeung Tsz Yan <yannieyeung@Yeungs-MacBook-Pro.local>
2020-02-27 15:25:00 +0800

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -2,16 +2,41 @@
 
 let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay =[];
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped" + " " + cardsInPlay[0]);
-
-let cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped" + " " + cardsInPlay[1]);
-
-if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again.");
+function checkForMatch () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  	console.log("You found a match!");
+	} else {
+  	console.log("Sorry, try again.");
+	}
 }
+
+function flipCard (cardId){
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch ();
+};
+flipCard (0);
+flipCard (2);
+
+
+
+
+
+
+
+	//if (cardsInPlay[0] === cardsInPlay[1]) {
+	//alert("You found a match!");
+	//} else {
+	//alert("Sorry, try again.");
+	//}
+
+//}
+
+
+	//let cardOne = cards[0];
+	//cardsInPlay.push(cardOne);
+	//console.log("User flipped" + " " + cardsInPlay[0]);
+
+	//let cardTwo = cards[1];
+	//cardsInPlay.push(cardTwo);
+	//console.log("User flipped" + " " + cardsInPlay[1]);
