a984ffcdca5dce553fd9de24173aebc8fe8853b2
Yeung Tsz Yan <yannieyeung@Yeungs-MacBook-Pro.local>
2020-03-03 23:02:33 +0800

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,6 +1,27 @@
 
 
-let cards = ["queen", "queen", "king", "king"];
+let cards = [ 
+	{ 
+		rank : "queen",
+		suit : "hearts",
+		cardImage : "images/queen-of-hearts.png"
+	},
+	{
+		rank : "queen",
+		suit : "diamonds",
+		cardImage : "images/queen-of-diamonds.png"	
+	},
+	{
+		rank : "king",
+		suit : "hearts",
+		cardImage : "images/king-of-hearts.png"
+	},
+	{
+		rank : "king",
+		suit : "diamonds",
+		cardImage : "images/king-of-diamonds.png"
+	}
+	];
 let cardsInPlay =[];
 function checkForMatch () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -11,12 +32,14 @@ function checkForMatch () {
 }
 
 function flipCard (cardId){
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch ();
 };
 flipCard (0);
-flipCard (2);
+flipCard (1);
 
 
 
